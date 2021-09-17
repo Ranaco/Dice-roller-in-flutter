0d66f7dc9537799c3b23907e52fbf1a64e9919fa
Ranaco <ranasatyamraj@gmail.com>
2021-09-17 21:41:21 +0530

    images inserted

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,24 @@
+// ignore_for_file: prefer_const_constructors, camel_case_types
 
+import 'package:flutter/material.dart';
+
+main(List<String> args) {
+  runApp(MaterialApp(
+    title: 'hello',
+    home: diceRoller(),
+  ));
+}
+
+class diceRoller extends StatefulWidget {
+  const diceRoller({Key? key}) : super(key: key);
+
+  @override
+  _diceRollerState createState() => _diceRollerState();
+}
+
+class _diceRollerState extends State<diceRoller> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
